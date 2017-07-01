568a2702c187323353862a71d5bb4d216df51959
Mikhail <supervajno@gmail.com>
2017-07-01 06:59:09 +0300

    comm1

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("sup brou")
-
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 def results(request, question_id):
